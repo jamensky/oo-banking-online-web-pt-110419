34b10526f5915bf24baf16cdba61499eab10a4f3
Jamal Rjoul <jamensky1991@gmail.com>
2019-12-22 21:25:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,8 @@ class Transfer
       receiver.balance += amount
       sender.balance -= amount
       @status = "complete"
+    else 
+      return "Transaction rejected. Please check your account balance."
     end
   end
 
